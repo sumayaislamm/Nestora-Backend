706672204795bbf473e5ccd2ec294878a967cd3e
Sumaya <sheikhsumaya622@gmail.com>
2026-07-07 20:04:43 +0600

    express setup in server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    }
+    catch (error) {
+        console.error(`Error starting server: ${error}`);
+        process.exit(1);
+
+    }
+}
+main();
