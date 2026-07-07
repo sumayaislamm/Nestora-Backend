490784793fdcc94eadc0e5e65a50a0567f76452a
Sumaya <sheikhsumaya622@gmail.com>
2026-07-07 22:09:50 +0600

    app and server file setting

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express, {Application, Request, Response} from "express";
 
-
-
 const app: Application = express();
 
 app.get("/", (req : Request, res : Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import app from "./app";
-const PORT = process.env.PORT || 5000;
+import { prisma } from "./lib/prisma";
+ import "dotenv/config";
+const PORT = process.env.PORT;
 
 async function main() {
 
     try {
+        // await prisma.$connect();
+        console.log("Prisma connected successfully");
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
     }
     catch (error) {
         console.error(`Error starting server: ${error}`);
+        // await prisma.$disconnect();
         process.exit(1);
 
     }
