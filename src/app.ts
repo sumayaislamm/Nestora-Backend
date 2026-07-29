54ba27c04535981d7113f1f1f998d2da7da1b32b
Sumaya <sheikhsumaya622@gmail.com>
2026-07-30 00:10:24 +0600

    file upload

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,8 @@ app.use(
         credentials: true,
     })
 );
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
